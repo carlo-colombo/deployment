e0e2ff767b9a394d5edd818799fdc441f5ad2819
Carlo Colombo <carlo.colombo@gmx.com>
2020-09-27 08:39:37 +0200

    Dashboard container with service
    - rework of variables to be more correct
    - generation of cookie and secret

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,18 @@
 
 set -exuo pipefail
 
+function random_string {
+  base32 --wrap=0 /dev/urandom | head -c 80
+}
+
 lpass sync
 
 lpass show "deployments/k3s/tiddlywiki" --notes \
-  | ytt -f spec/  -f - \
-    | kbld -f - 2> /dev/null \
-    | kbld relocate -f - --repository rg.nl-ams.scw.cloud/carlo-colombo/tiddlywiki-images \
+    | ytt -f spec/  -f - \
+    --data-value release.cookie="$(random_string)" \
+    --data-value release.secret_key_base="$(random_string)" \
+    | kbld -f - \
     | kapp deploy --app tiddlywiki  \
-           -f - \
-           --diff-changes \
-           --yes
-
+    -f - \
+    --diff-changes \
+    --yes
