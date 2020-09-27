3d138ce3f0e4b907e96261e90e5b11bbde75d77b
Carlo Colombo <carlo.colombo@gmx.com>
2020-09-27 08:42:54 +0200

    Removed original send-to-wiki bot

--- send-to-wiki/deps.ts
+++
@@ -1,3 +0,0 @@
-export { listenAndServe } from "https://deno.land/std/http/server.ts"
-export { decoder } from "https://deno.land/std/encoding/utf8.ts";
-export * as log from "https://deno.land/std/log/mod.ts"
