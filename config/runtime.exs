b18d2dc144144ba23d66fd1f3c00e88bab1b03f7
Carlo Colombo <carlo.colombo@gmx.com>
2021-01-12 18:45:37 +0100

    added missing config

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,3 +16,17 @@ config :libcluster, topologies: [
 
 config :feed2wiki, bot_name: "tiddlywiki_bot"
 
+config :nadia,
+  token: System.get_env("TELEGRAM_BOT_TOKEN")
+
+config :tiddlywiki_bot,
+  extract_info_url: System.get_env("EXTRACT_INFO_URL"),
+  tasks_filter: "[tag[chandler]!tag[done]]",
+  register: System.get_env("REGISTER_ADDRESS", "https://api.telegram.org"),
+  own: System.get_env("OWN_ADDRESS"),
+  wiki: [
+    url: System.get_env("WIKI_URL"),
+    username: System.get_env("WIKI_USERNAME"),
+    password: System.get_env("WIKI_PASSWORD"),
+    external_url: System.get_env("WIKI_EXTERNAL_URL")
+  ]
