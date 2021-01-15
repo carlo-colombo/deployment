dcdf05180f00f775adfaae2c75c0bf49be510405
Carlo Colombo <carlo.colombo@gmx.com>
2021-01-15 19:30:56 +0100

    adding mfa to logs

--- config/config.exs
+++ config/config.exs
@@ -32,3 +32,7 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :logger, :console,
+ format: "[$level] $message $metadata\n",
+ metadata: [:mfa]
